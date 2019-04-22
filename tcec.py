d61dd604bb5c56b1f0a428d740c41d28bec1ccb2
Oleg Smirnov <sovaz1997@yandex.ru>
2019-04-22 17:31:43 +0300

    final (not fully optimized)

--- tcec.py
+++ tcec.py
@@ -1,6 +1,10 @@
 from urllib.request import urlopen
 import json
 import random
+import copy
+import numpy
+import cProfile
+import os.path
 
 eloAdvantage = 32.8
 eloDraw = 97.3
@@ -20,34 +24,33 @@ class Game:
         self.result = result
         self.played = played
     
-    def simulate():
-        if not self.played:
-            goodness = 1000000000
-            res = random.randint(0, goodness)
+    def simulate(self):
+        goodness = 1000000000
+        res = random.randint(0, goodness)
 
-            whiteWin = whiteProbability()
-            blackWin = blackProbability()
+        whiteWin = self.whiteProbability()
+        blackWin = self.blackProbability()
 
-            if res <= goodness * whiteWin:
-                self.result = 1
-            elif res <= goodness * blackWin:
-                self.result = 0
-            else:
-                self.result = 0.5
+        if res <= goodness * whiteWin:
+            self.result = 1
+        elif res <= goodness * whiteWin + goodness * blackWin:
+            self.result = 0
+        else:
+            self.result = 0.5
 
-            self.played = True
+        self.played = True
     
     def clearResult(self):
         self.played = False
     
-    def function(delta):
+    def function(self, delta):
         return 1 / (1 + 10 ** (delta / 400))
     
     def whiteProbability(self):
-        return f(self.black_engine.engine_elo - self.white_engine.white_elo - eloAdvantage + eloDraw)
+        return self.function(self.black_engine.engine_elo - self.white_engine.engine_elo - eloAdvantage + eloDraw)
     
     def blackProbability(self):
-        return f(self.white_engine.engine_elo - self.black_engine.white_elo + eloAdvantage + eloDraw)
+        return self.function(self.white_engine.engine_elo - self.black_engine.engine_elo + eloAdvantage + eloDraw)
 
     def __str__(self):
         res = str(self.white_engine) + '\n' + str(self.black_engine)
@@ -70,19 +73,83 @@ def calculatePositions(games):
     scores = {}
 
     for i in games:
-        if i.played
-            if not i.white_engine.engine_name in result:
-                scores[i.white_engine.engine_name] = 0
+        if i.white_engine.engine_name not in scores:
+            scores[i.white_engine.engine_name] = 0
+        if i.black_engine.engine_name not in scores:
+            scores[i.black_engine.engine_name] = 0
 
-            if i.result == 1:
-                scores[i.white_engine.engine_name] += 1
-            elif i.result == 0:
-                scores[i.black_engine.engine_name] += 1
-            else:
-                scores[i.white_engine.engine_name] += 0.5
-                scores[i.black_engine.engine_name] += 0.5
+        if i.result == 1:
+            scores[i.white_engine.engine_name] += 1
+        elif i.result == 0:
+            scores[i.black_engine.engine_name] += 1
+        else:
+            scores[i.white_engine.engine_name] += 0.5
+            scores[i.black_engine.engine_name] += 0.5
+    
+    result = []
 
+    for i in sorted(scores.items(), key=lambda kv: kv[1], reverse=True):
+        result.append(i[0])
 
+    return result
+    
+
+def makeSimulations(games, engines, cnt):
+    result = {}
+
+    for i in engines:
+        result[i] = numpy.zeros(len(engines))
+    print(result)
+
+    playedArray = numpy.zeros(len(games), dtype=bool)
+    
+    for i in range(len(games)):
+        playedArray[i] = games[i].played
+
+    for i in range(cnt):
+        for j in range(len(games)):
+            if not playedArray[j]:
+                games[j].simulate()
+
+        positions = calculatePositions(games)
+        
+        for j in range(len(positions)):
+            result[positions[j]][j] += 1
+    
+    for i in sorted(result, key=lambda x: result[x].argmax()):
+        
+        print("{:<30}".format(i) + ": ", end=' ')
+        for j in result[i]:
+            print("{:07.4f}".format(j / cnt * 100), end=' ')
+        print('')
+
+def setEnginesInfo(engine_names, filename):
+    data = {}
+
+    print('Введите рейтинги движков:')
+    
+    for i in engine_names:
+        print(i + ": ", end='')
+        rating = int(input())
+        data[i] = rating
+    
+    with open(filename, 'w') as outfile:
+        json.dump(data, outfile)
+
+def installRatingEngines(engine_names, filename):
+    engines = {}
+
+    if not os.path.isfile(filename):
+        setEnginesInfo(engine_names, filename)
+
+    # Сопоставления движка и его рейтинга
+    with open(filename, 'r') as data_file:
+        data = json.load(data_file)
+
+    for i in data:
+        engines[i] = Engine(i, data[i])
+
+    return engines
 
 schedule = json.loads(getSchedule()) # получение расписания
 engines_names = set()
@@ -92,12 +159,7 @@ for i in schedule:
     engines_names.add(i['White'])
     engines_names.add(i['Black'])
 
-
-engines = {}
-
-# Сопоставления движка и его рейтинга
-for i in engines_names:
-    engines[i] = Engine(i, 2500)
+engines = installRatingEngines(engines_names, 'engines.json')
 
 for i in engines:
     print(i)
@@ -109,10 +171,10 @@ for i in schedule:
         games.append(Game(engines[i['White']], engines[i['Black']]))
     else:
         if i['Result'] == '1-0':
-            games.append(Game(i['White'], i['Black'], 1, 1))
+            games.append(Game(engines[i['White']], engines[i['Black']], 1, 1))
         elif i['Result'] == '0-1':
-            games.append(Game(i['White'], i['Black'], 0, 1))
+            games.append(Game(engines[i['White']], engines[i['Black']], 0, 1))
         else:
-            games.append(Game(i['White'], i['Black'], 0.5, 1))
+            games.append(Game(engines[i['White']], engines[i['Black']], 0.5, 1))
 
-
+makeSimulations(games, engines, 10000)
