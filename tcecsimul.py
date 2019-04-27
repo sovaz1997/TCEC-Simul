448700520881ccffc221867b26407107caea58ab
Oleg Smirnov <sovaz1997@yandex.ru>
2019-04-27 11:34:45 +0300

    remove GUI code

--- tcecsimul.py
+++ tcecsimul.py
@@ -8,9 +8,6 @@ import math
 import sys
 import csv
 
-from PySide2.QtWidgets import QApplication
-from PySide2.QtQuick import QQuickView
-from PySide2.QtCore import QUrl
 from progressbar import ProgressBar, Percentage, Bar, ETA
 
 from options import options
